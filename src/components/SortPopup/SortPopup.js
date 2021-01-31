9b7b9d2d97546dc362bec7153ece930461c42867
ZeR018 <varvarin262@gmail.com>
2021-02-01 00:48:51 +0300

    cart

--- src/components/SortPopup/SortPopup.js
+++ src/components/SortPopup/SortPopup.js
@@ -14,7 +14,8 @@ const SortPopup = ({ items }) => {
 	};
 	const sortRef = useRef();
 	const handleOutsideClick = (e) => {
-		if (!e.path.includes(sortRef.current)) {
+		const path = e.path || (e.composedPath && e.composedPath());
+		if (!path.includes(sortRef.current)) {
 			setVisiblePopup(false);
 		}
 	};
