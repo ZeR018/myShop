9988318f9efc54c57317652539e4535fba54aabb
ZeR018 <varvarin262@gmail.com>
2021-02-18 15:49:47 +0300

    .

--- src/App/App.js
+++ src/App/App.js
@@ -10,13 +10,6 @@ import Footer from '../components/Footer';
 
 import { fetchWeather } from '../store/actions/weather';
 
-// http://localhost:3000/db.json
-//const dbLink = 'http://localhost:3000/db.json';
-
-// ссылка для Апи через выбранный город http://api.openweathermap.org/data/2.5/weather?q=${selectedCity},ru&appid=${weatherAPIKey}
-
-// URL значка погоды `http://openweathermap.org/img/wn/${iconCode}@2x.png`
-
 const App = () => {
 	const dispatch = useDispatch();
 	React.useEffect(() => {
